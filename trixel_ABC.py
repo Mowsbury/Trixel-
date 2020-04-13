66533bdf033b11ac2da43c6686c6e0ceae4bff0f
Green <52164526+Mowsbury@users.noreply.github.com>
2020-04-13 08:00:26 -0600

    Add files via upload

---
+++ trixel_ABC.py
@@ -0,0 +1,9 @@
+# By  Green Mowsbury
+# https://github.com/Mowsbury/Trixel-
+
+import numpy as np
+data_car=[[[120,23,25],[214,25,94],[140,121,90]],[[120,23,25],[214,25,94],[140,121,90]],[[120,23,25],[214,25,94],[140,121,90]]]
+trx1=np.array((data_car))
+
+
+print(trx1)
